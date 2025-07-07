fc85128dafc3a70947ab745248c46c40d72f32ae
KelzangUD <kuding755@gmail.com>
2025-07-07 12:38:25 +0600

    posthog integration

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { StyledEngineProvider } from "@mui/material";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: process.env.REACT_APP_POSTHOG_HOST,
+  // defaults: "2025-05-24",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <App />
+      <PostHogProvider
+        apiKey={process.env.REACT_APP_POSTHOG_API_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
     </StyledEngineProvider>
   </React.StrictMode>
 );
